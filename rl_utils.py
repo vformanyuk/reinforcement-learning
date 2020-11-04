237a64fefc317383d08f1f4cd94635832880da4a
Vadim Formanyuk <vformanyuk@gmail.com>
2020-11-04 13:48:59 +0300

    Prioritized expirience replay with importance sampling on custom replay buffer built on sum tree

--- rl_utils.py
+++ rl_utils.py
@@ -147,39 +147,3 @@ class SARST_TD_Priority_MemoryBuffer(object):
             tf.stack(self.dones_memory[idxs]), \
             tf.convert_to_tensor(importance_sampling_weights, dtype=tf.float32), \
             idxs
-
-class SARST_Rank_Priority_MemoryBuffer(object):
-    def __init__(self, buffer_size, state_shape, action_shape, action_type = np.float32, alpha=0.6, beta=0.4, beta_inc_rate=1.0001):
-        self.states_memory = np.empty(shape=(buffer_size, *state_shape), dtype = np.float32)
-        self.next_states_memory = np.empty(shape=(buffer_size, *state_shape), dtype = np.float32)
-        self.actions_memory = np.empty(shape=(buffer_size, *action_shape), dtype = action_type)
-        self.rewards_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
-        self.dones_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
-        self.buffer_size = buffer_size
-        self.memory_idx = 0
-        self.max_td = 1
-        self.total = 0
-        self.alpha = alpha
-        self.beta = beta
-        self.beta_increase_rate = beta_inc_rate
-
-    def store(self, state:tf.Tensor, action:tf.Tensor, next_state:tf.Tensor, reward:tf.Tensor, is_terminal:tf.Tensor):
-        write_idx = self.memory_idx % self.buffer_size
-        self.states_memory[write_idx] = state
-        self.next_states_memory[write_idx] = next_state
-        self.actions_memory[write_idx] = action
-        self.rewards_memory[write_idx] = reward
-        self.dones_memory[write_idx] = is_terminal
-        self.memory_idx += 1
-
-    def update_priorities(self, idxs, td_errors):
-        pass
-
-    def __call__(self, batch_size):
-        upper_bound = self.memory_idx if self.memory_idx < self.buffer_size else self.buffer_size
-        idxs = np.random.permutation(upper_bound)[:batch_size]
-        return tf.stack(self.states_memory[idxs]), \
-            tf.stack(self.actions_memory[idxs]), \
-            tf.stack(self.next_states_memory[idxs]), \
-            tf.stack(self.rewards_memory[idxs]), \
-            tf.stack(self.dones_memory[idxs])
