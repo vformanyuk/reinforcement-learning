de39670938c6c591e231b89e1fd63ef4725cd7af
Vadim Formanyuk <vformanyuk@gmail.com>
2021-10-03 16:06:58 +0300

    R2D2 replay buffer implementation. Random uniform sampling - i.e. no importance sampling

--- rl_utils/SAR_RNN_MemoryBuffer.py
+++ rl_utils/SAR_RNN_MemoryBuffer.py
@@ -4,33 +4,43 @@ import tensorflow as tf
 
 from itertools import chain
 
-class SARST_NStepReturn_RandomAccess_MemoryBuffer(object):
-    def __init__(self, buffer_size, trajectory_size, trajectory_overlap, N, gamma, state_shape, action_shape, action_type = np.float32):
+class SAR_NStepReturn_RandomAccess_MemoryBuffer(object):
+    def __init__(self, buffer_size, N, gamma, state_shape, action_shape, reward_shape=None, action_type = np.float32, \
+                trajectory_size=80, trajectory_overlap=40, burn_in_length=40):
         self._trajectory_size = trajectory_size
         self._trajectory_overlap = trajectory_overlap
+        self._burn_in_len = burn_in_length
         self.states_memory = np.empty(shape=(buffer_size, *state_shape), dtype = np.float32)
-        self.next_states_memory = np.empty(shape=(buffer_size, *state_shape), dtype = np.float32)
         self.actions_memory = np.empty(shape=(buffer_size, *action_shape), dtype = action_type)
-        self.rewards_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
+        real_reward_shape = (buffer_size,) if reward_shape == None else (buffer_size, *reward_shape)
+        self.rewards_memory = np.empty(shape=real_reward_shape, dtype = np.float32)
         self.gamma_power_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
         self.dones_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
+        self.actor_hidden_states_memory = []
+        self.critic_hidden_states_memory = []
         self.buffer_size = buffer_size
         self.memory_idx = 0
-        self.trajectory_idx = 0
         self.N = N
         self.current_trajectory = []
         self.overlapping_trajectory = []
-        self.trajectory_store = [] #list of np arrays
+        self.burn_in_trajectory = []
+        self.trajectory_store = []
+        self.burn_in_store = []
+        self.collecting_burn_in = True
+        self.hidden_state_idx = 0
         self.gammas=[]
         self.gammas.append(1) # gamma**0
         self.gammas.append(gamma) #gamma**1
         for i in range(2, N + 1):
             self.gammas.append(np.power(gamma,i))
 
-    def store(self, state:tf.Tensor, action:tf.Tensor, next_state:tf.Tensor, reward:tf.Tensor, is_terminal:tf.Tensor):
+    def _store_hidded_state(self, actor_hidden_state:tf.Tensor, critic_hidden_state:tf.Tensor):
+        self.actor_hidden_states_memory.append(actor_hidden_state)
+        self.critic_hidden_states_memory.append(critic_hidden_state)
+
+    def store(self, actor_hidden_state:tf.Tensor, critic_hidden_state:tf.Tensor, state:tf.Tensor, action:tf.Tensor, reward:tf.Tensor, is_terminal:tf.Tensor):
         write_idx = self.memory_idx % self.buffer_size
         self.states_memory[write_idx] = state
-        self.next_states_memory[write_idx] = next_state
         self.actions_memory[write_idx] = action
         self.rewards_memory[write_idx] = 0 # will be set in a loop
         self.gamma_power_memory[write_idx] = 0
@@ -46,45 +56,45 @@ class SARST_NStepReturn_RandomAccess_MemoryBuffer(object):
             n_return_idx += 1
             update_idx = (self.buffer_size + (write_idx - n_return_idx)) % self.buffer_size
 
-        self.current_trajectory.append(self.memory_idx)
-        if len(self.current_trajectory) == self._trajectory_size or is_terminal.numpy() > 0.5: # trajectory shouldn't overlap episode
-            self.trajectory_store.append(np.array(self.current_trajectory, dtype=np.int))
-            self.current_trajectory = self.overlapping_trajectory
+        if not self.collecting_burn_in or self._burn_in_len == 0: # Burn-in trajectory must be filled up first if used
+            self.current_trajectory.append(write_idx)
+
+        if len(self.current_trajectory) == self._trajectory_size or is_terminal > 0.5: # trajectory shouldn't overlap episode
+            self.trajectory_store.append(np.array(self.current_trajectory[:], dtype=np.int)) # trajectory length is less then expected
+            self.current_trajectory = self.overlapping_trajectory[:]
             self.overlapping_trajectory = []
-        if len(self.current_trajectory) >= (self._trajectory_size - self._trajectory_overlap):
-            self.overlapping_trajectory.append(self.memory_idx)
+
+        if len(self.current_trajectory) >= (self._trajectory_size - self._trajectory_overlap): # begin collecting overlaping trajectory
+            self.overlapping_trajectory.append(write_idx)
+        
+        if  len(self.current_trajectory) == (self._trajectory_size - self._burn_in_len  - self._trajectory_overlap):
+            self.burn_in_trajectory.clear() # clear exisitng burn-in trajectory to start collecting new one
+        
+        if self._burn_in_len > 0 and len(self.burn_in_trajectory) < self._burn_in_len:
+            self.burn_in_trajectory.append(write_idx)
+            if len(self.burn_in_trajectory) == 1: # store hidden states for burn-in trajectory unroll
+                self._store_hidded_state(actor_hidden_state, critic_hidden_state)
+            if len(self.burn_in_trajectory) == self._burn_in_len: # save burn-in trajectory and begin collecting training trajectory
+                self.burn_in_store.append(np.array(self.burn_in_trajectory[:], dtype=np.int))
+                self.current_trajectory.append(write_idx) # last burn-in trajectory record is first one of training trajectory
+                self.collecting_burn_in = False
+
+        if is_terminal > 0:
+            self.current_trajectory.clear()
+            self.overlapping_trajectory.clear()
+            self.burn_in_trajectory.clear()
+            self.collecting_burn_in = True
 
         self.memory_idx += 1
 
-    def reset(self):
-        self.memory_idx = 0
-
-    def get_tail_batch(self, transfer_len):
-        upper_bound = self.memory_idx % self.buffer_size
-        lower_bound = upper_bound - transfer_len
-        tail_interval = 0
-        if lower_bound < 0:
-            tail_interval = np.abs(lower_bound)
-            lower_bound = 0
-            
-        idxs = range(lower_bound, upper_bound)
-        if tail_interval > 0:
-            idxs = list(chain(idxs, range(self.buffer_size - tail_interval, self.buffer_size)))
-        return tf.stack(self.states_memory[idxs]), \
-            tf.stack(self.actions_memory[idxs]), \
-            tf.stack(self.next_states_memory[idxs]), \
-            tf.stack(self.rewards_memory[idxs]), \
-            tf.stack(self.gamma_power_memory[idxs]), \
-            tf.stack(self.dones_memory[idxs])
-
     def __call__(self, batch_size):
-        upper_bound = (self.memory_idx - 1) if self.memory_idx < self.buffer_size else (self.buffer_size - 1)
-        if self.dones_memory[upper_bound] < 1:
-            upper_bound -= self.N # last N records don't have full n-step return calculated, unless it is a terminal state.
-        idxs = np.random.permutation(upper_bound)[:batch_size]
-        return tf.stack(self.states_memory[idxs]), \
-            tf.stack(self.actions_memory[idxs]), \
-            tf.stack(self.next_states_memory[idxs]), \
-            tf.stack(self.rewards_memory[idxs]), \
-            tf.stack(self.gamma_power_memory[idxs]), \
-            tf.stack(self.dones_memory[idxs])
+        idxs = np.random.permutation(len(self.trajectory_store))[:batch_size]
+        for idx in idxs:
+            yield self.actor_hidden_states_memory[idx], self.critic_hidden_states_memory[idx], \
+                tf.stack(self.states_memory[self.burn_in_store[idx]]), \
+                tf.stack(self.actions_memory[self.burn_in_store[idx]]), \
+                tf.stack(self.states_memory[self.trajectory_store[idx]]), \
+                tf.stack(self.actions_memory[self.trajectory_store[idx]]), \
+                tf.stack(self.rewards_memory[self.trajectory_store[idx]]), \
+                tf.stack(self.gamma_power_memory[self.trajectory_store[idx]]), \
+                tf.stack(self.dones_memory[self.trajectory_store[idx]])
