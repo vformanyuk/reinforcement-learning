cdee6375ead268e5541cc74465bc12f49b691937
Vadim Formanyuk <vformanyuk@gmail.com>
2022-04-29 15:38:59 +0300

    Improve trajectories colleciton. Learning stabilized

--- R2D2/R2D2_AgentBuffer.py
+++ R2D2/R2D2_AgentBuffer.py
@@ -1,12 +1,45 @@
 import numpy as np
 import tensorflow as tf
 
+class Trajectory:
+    def __init__(self, trajectory_length:int, burn_in_length:int):
+        self.burn_in = np.zeros(burn_in_length, dtype=np.int32) if burn_in_length > 0 else []
+        self.data = np.zeros(trajectory_length, dtype=np.int32)
+        self.write_idx = 0
+        self.writing_burn_in = burn_in_length > 0
+
+    def add(self, idx):
+        if self.is_complete():
+            return
+
+        if self.writing_burn_in:
+            self.burn_in[self.write_idx] = idx
+        else:
+            self.data[self.write_idx] = idx
+
+        self.write_idx += 1
+        if self.writing_burn_in and self.write_idx == len(self.burn_in):
+            self.writing_burn_in = False
+            self.data[0] = self.burn_in[-1]
+            self.write_idx = 1
+
+    def is_complete(self) -> bool:
+        return self.write_idx >= len(self.data)
+
+    def verify_trajectory(self):
+        # trajectory lenght must be at least 2. If it's lesss then take one more record from burn in
+        if self.writing_burn_in or self.write_idx > 1:
+            return
+        self.data[1] = self.burn_in[-1]
+        self.data[0] = self.burn_in[-2]
+
 class R2D2_AgentBuffer(object):
     def __init__(self, distributed_mode:bool, buffer_size:int, N:int, gamma:float, 
                 state_shape, action_shape, hidden_state_shape, reward_shape=None, action_type = np.float32,
-                trajectory_size=80, burn_in_length=40):
+                trajectory_size=80, burn_in_length=10, trajectory_overlap = 40):
         self._distributed_mode = distributed_mode # when NOT in distributed mode trajectory cache and burn-in memory not cleared after episode end
         self._trajectory_size = trajectory_size
+        self._trajectory_overlap = trajectory_overlap
         self._burn_in_len = burn_in_length
         self.buffer_size = buffer_size
         self.states_memory = np.empty(shape=(buffer_size, *state_shape), dtype = np.float32)
@@ -16,12 +49,10 @@ class R2D2_AgentBuffer(object):
         self.gamma_power_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
         self.dones_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
         self.actor_hidden_states_memory = []
-        self.burn_in_memory = []
-        self.trajectory_cache = []
+        self.trajectories = []
+        self.trajectories.append(Trajectory(self._trajectory_size,0))
         self.memory_idx = 0
         self.N = N
-        self.current_trajectory = []
-        self.burn_in_trajectory = []
         self.gammas=[]
         self.gammas.append(1) # gamma**0
         self.gammas.append(gamma) #gamma**1
@@ -29,7 +60,7 @@ class R2D2_AgentBuffer(object):
             self.gammas.append(np.power(gamma,i))
 
     def __len__(self):
-        return len(self.trajectory_cache)
+        return len(self.trajectories)
 
     '''
     Pack (state, action, reward) tuples into solid trajectory. 
@@ -50,91 +81,41 @@ class R2D2_AgentBuffer(object):
             self.gamma_power_memory[self.memory_idx - n_return_idx] = n_return_idx
             n_return_idx += 1
 
-        self.current_trajectory.append(self.memory_idx)
-        if self.memory_idx == 0:
-            self.__store_burn_in([])
-        
-        if  len(self.current_trajectory) == (self._trajectory_size - self._burn_in_len):
-            self.burn_in_trajectory.clear() # clear exisitng burn-in trajectory to start collecting new one
-        
-        if self._burn_in_len > 0 and len(self.burn_in_trajectory) < self._burn_in_len and self.memory_idx >= (self._trajectory_size - self._burn_in_len + 1):
-            self.burn_in_trajectory.append(self.memory_idx)
-            if len(self.burn_in_trajectory) == self._burn_in_len: # save burn-in trajectory and begin collecting training trajectory
-                self.__store_burn_in(self.burn_in_trajectory) # don't clear collected trajectory here                
-                self.current_trajectory.append(self.memory_idx) # last burn-in trajectory record is first one of training trajectory
+        for trajectory in self.trajectories:
+            trajectory.add(self.memory_idx)
+            if is_terminal > 0:
+                trajectory.verify_trajectory()
 
-        if len(self.current_trajectory) == self._trajectory_size or is_terminal > 0: # trajectory shouldn't overlap episode
-            if len(self.current_trajectory) == 1: # trajectory must have at least legth of 2
-                # if current trajectory length is 1, then burn-in memeory can not contain redundant records
-                # thus, it is safe to take preciding state from burn-in to train trajectory
-                trajectory_idx = self.current_trajectory[0]
-                self.current_trajectory.insert(0, trajectory_idx - 1) #insert previous item index
-            self.__cache(self.current_trajectory)
-            self.current_trajectory.clear()
-            if is_terminal > 0: # this part is redundant for R2D2 agent because it's reset afte each played episode
-                redundant_records_count = len(self.burn_in_memory) - len(self.trajectory_cache) if self._burn_in_len > 0 else 0
-                assert redundant_records_count >= 0, "To few burn-in trajectories"
-                # burn-in and hidden states storages might contain redundant records.
-                # Only trajectory store contains correct number of records
-                for _ in range(redundant_records_count):
-                    self.burn_in_memory.pop()
-                self.reset()
-                return
+        if self.memory_idx > 0 and \
+            self.memory_idx % (self._trajectory_size - self._burn_in_len - self._trajectory_overlap) == 0 and \
+            not is_terminal > 0:
+            self.trajectories.append(Trajectory(self._trajectory_size, self._burn_in_len))
+
         self.memory_idx += 1
 
     def reset(self):
         if self._distributed_mode: # in distribured mode (for APE-X or R2D2) memory completly cleared after every episode
-            self.burn_in_memory.clear()
             self.actor_hidden_states_memory.clear()
-            self.trajectory_cache.clear()
-        self.current_trajectory.clear()
-        self.burn_in_trajectory.clear()
+            self.trajectories.clear()
+            self.trajectories.append(Trajectory(self._trajectory_size,0))
         self.memory_idx = 0
 
-    def __store_burn_in(self, burn_in):
-        burn_in_trajectory = []
-        for idx in burn_in:
-            burn_in_trajectory.append(tf.convert_to_tensor(self.states_memory[idx], dtype=tf.float32))
-        self.burn_in_memory.append(burn_in_trajectory)
-
-    def __cache(self, trajectory):
-        states_idxs = trajectory[:-1]
-        assert len(states_idxs) > 0, "Bad trajectory. \"states_idxs\" length=0 when caching trajectory"
-        trajectory_idxs = trajectory[1:]
-        assert len(trajectory_idxs) > 0, "Bad trajectory. \"trajectory_idxs\" length=0 when caching trajectory"
-        states_ = tf.stack(self.states_memory[states_idxs])
-        actions_ = tf.stack(self.actions_memory[trajectory_idxs])
-        next_states_ = tf.stack(self.states_memory[trajectory_idxs])
-        rewards_ = tf.stack(self.rewards_memory[trajectory_idxs])
-        gps_ = tf.stack(self.gamma_power_memory[trajectory_idxs])
-        dones_ = tf.stack(self.dones_memory[trajectory_idxs])
-        hidden_states_ = tf.stack([tf.squeeze(self.actor_hidden_states_memory[idx], axis=0) for idx in states_idxs])
-        self.trajectory_cache.append((states_, actions_, next_states_, rewards_, gps_, dones_, hidden_states_))
-
     def get_tail(self, batch_size):
-        upper_bound = len(self.trajectory_cache)
+        upper_bound = len(self.trajectories)
         lower_bound = 0 if upper_bound == 0 else upper_bound - batch_size
         assert lower_bound >= 0, "Too few records in trajectory cache"
         for idx in range(lower_bound, upper_bound):
-            yield self.actor_hidden_states_memory[idx], \
-                    self.burn_in_memory[idx], \
-                    self.trajectory_cache[idx][0], \
-                    self.trajectory_cache[idx][1], \
-                    self.trajectory_cache[idx][2], \
-                    self.trajectory_cache[idx][3], \
-                    self.trajectory_cache[idx][4], \
-                    self.trajectory_cache[idx][5], \
-                    self.trajectory_cache[idx][6]
-
-    def __call__(self, batch_size):
-        idxs = np.random.permutation(len(self.trajectory_cache))[:batch_size]
-        for idx in idxs:
-            yield self.actor_hidden_states_memory[idx], \
-                    self.burn_in_memory[idx], \
-                    self.trajectory_cache[idx][0], \
-                    self.trajectory_cache[idx][1], \
-                    self.trajectory_cache[idx][2], \
-                    self.trajectory_cache[idx][3], \
-                    self.trajectory_cache[idx][4], \
-                    self.trajectory_cache[idx][5], \
-                    self.trajectory_cache[idx][6]
+            hidden_state_idx = self.trajectories[idx].data[1] # [0] is last in burn-in
+            burn_in_idxs = self.trajectories[idx].burn_in
+            states_idxs = self.trajectories[idx].data[:-1]
+            trajectory_idxs = self.trajectories[idx].data[1:]
+            burn_in_trajectory = tf.stack(self.states_memory[burn_in_idxs]) if len(burn_in_idxs) > 0 else []
+            yield self.actor_hidden_states_memory[hidden_state_idx], \
+                    burn_in_trajectory, \
+                    tf.stack(self.states_memory[states_idxs]), \
+                    tf.stack(self.actions_memory[trajectory_idxs]), \
+                    tf.stack(self.states_memory[trajectory_idxs]), \
+                    tf.stack(self.rewards_memory[trajectory_idxs]), \
+                    tf.stack(self.gamma_power_memory[trajectory_idxs]), \
+                    tf.stack(self.dones_memory[trajectory_idxs]), \
+                    tf.stack([tf.squeeze(self.actor_hidden_states_memory[i], axis=0) for i in states_idxs])
