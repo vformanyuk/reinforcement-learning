3ea95c2f10febedf9d9c1de1dd6a1a14bccde16a
Vadim Formanyuk <vformanyuk@gmail.com>
2022-05-16 00:30:51 +0300

    Local trajectory memory fixes

--- R2D2/R2D2_AgentBuffer.py
+++ R2D2/R2D2_AgentBuffer.py
@@ -2,11 +2,12 @@ import numpy as np
 import tensorflow as tf
 
 class Trajectory:
-    def __init__(self, trajectory_length:int, burn_in_length:int):
+    def __init__(self, N:int, trajectory_length:int, burn_in_length:int):
         self.burn_in = np.zeros(burn_in_length, dtype=np.int32) if burn_in_length > 0 else []
         self.data = np.zeros(trajectory_length, dtype=np.int32)
         self.write_idx = 0
         self.writing_burn_in = burn_in_length > 0
+        self.n_backup = N
 
     def add(self, idx):
         if self.is_complete():
@@ -26,12 +27,22 @@ class Trajectory:
     def is_complete(self) -> bool:
         return self.write_idx >= len(self.data)
 
-    def verify_trajectory(self):
-        # trajectory lenght must be at least 2. If it's lesss then take one more record from burn in
-        if self.writing_burn_in or self.write_idx > 1:
+    def is_n_backup_complete(self, buffer_idx) -> bool:
+        return self.write_idx >= len(self.data) and self.data[-1] + self.n_backup >= buffer_idx
+
+    def fix_trajectory(self):
+        if self.writing_burn_in:
             return
-        self.data[1] = self.burn_in[-1]
-        self.data[0] = self.burn_in[-2]
+        first_zero = -1
+        for i,d in enumerate(self.data):
+            if d == 0:
+                first_zero = i
+                break
+        if first_zero == 1: # trajectory lenght must be at least 2. If it's less then take one more record from burn in
+            self.data[1] = self.burn_in[-1]
+            self.data[0] = self.burn_in[-2]
+            first_zero += 1
+        self.data = self.data[:first_zero] # truncate trailing zeros
 
 class R2D2_AgentBuffer(object):
     def __init__(self, distributed_mode:bool, buffer_size:int, N:int, gamma:float, 
@@ -50,7 +61,7 @@ class R2D2_AgentBuffer(object):
         self.dones_memory = np.empty(shape=(buffer_size,), dtype = np.float32)
         self.actor_hidden_states_memory = []
         self.trajectories = []
-        self.trajectories.append(Trajectory(self._trajectory_size,0))
+        self.trajectories.append(Trajectory(N, self._trajectory_size,0))
         self.sent_trajectories = []
         self.trajectory_ready = trajectory_ready_callback
         self.memory_idx = 0
@@ -85,15 +96,14 @@ class R2D2_AgentBuffer(object):
 
         for idx, trajectory in enumerate(self.trajectories):
             trajectory.add(self.memory_idx)
-            if self.trajectory_ready!=None and idx not in self.sent_trajectories and \
-               trajectory.is_complete() and trajectory.write_idx + self.N >= self.memory_idx:
+            if self.trajectory_ready != None and idx not in self.sent_trajectories and trajectory.is_n_backup_complete(self.memory_idx):
                 self.trajectory_ready(self, idx)
                 self.sent_trajectories.append(idx)
 
         if self.memory_idx > 0 and \
             self.memory_idx % (self._trajectory_size - self._burn_in_len - self._trajectory_overlap) == 0 and \
             not is_terminal > 0:
-            self.trajectories.append(Trajectory(self._trajectory_size, self._burn_in_len))
+            self.trajectories.append(Trajectory(self.N, self._trajectory_size, self._burn_in_len))
 
         self.memory_idx += 1
 
@@ -102,14 +112,16 @@ class R2D2_AgentBuffer(object):
             self.actor_hidden_states_memory.clear()
             self.sent_trajectories.clear()
             self.trajectories.clear()
-            self.trajectories.append(Trajectory(self._trajectory_size,0))
+            self.trajectories.append(Trajectory(self.N, self._trajectory_size, 0))
         self.memory_idx = 0
 
     def get_remaining_trajectories(self):
         for idx, trajectory in enumerate(self.trajectories):
             if idx not in self.sent_trajectories and not trajectory.writing_burn_in:
-                trajectory.verify_trajectory()
+                trajectory.fix_trajectory()
                 yield idx
+                if not trajectory.is_complete(): # stop producing trajectories after first incomplete met
+                    break
 
     def get_data(self, trajectory_idxs):
         for idx in trajectory_idxs:
